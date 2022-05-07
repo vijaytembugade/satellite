9c08eef969524b5607e21f198f8c0790cb4ff4cc
vijaytembugade <vijaytembugade21@gmail.com>
2022-05-07 18:48:54 +0530

    user data show

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,5 +1,9 @@
+import { useEffect, useReducer } from 'react';
+import { useAuth } from '../contexts';
 import {
+  Box,
   Button,
+  DrawerOverlay,
   Flex,
   FormControl,
   FormLabel,
@@ -7,51 +11,247 @@ import {
   Text,
 } from '@chakra-ui/react';
 import React from 'react';
+import Creatable from 'react-select/creatable';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  updateDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase/config';
+const options = [
+  { value: 'Javscript', label: 'Javscript' },
+  { value: 'React', label: 'React' },
+  { value: 'Html', label: 'Html' },
+  { value: 'CSS', label: 'CSS' },
+  { value: 'Angular', label: 'Angular' },
+  { value: 'Vuejs', label: 'Vuejs' },
+  { value: 'C++', label: 'C++' },
+  { value: 'C', label: 'C' },
+  { value: 'Python', label: 'Python' },
+  { value: 'NextJs', label: 'NextJs' },
+  { value: 'Tailwind', label: 'Tailwind' },
+  { value: 'Git', label: 'Git' },
+];
 
 export const ProfileDetails = ({ close }) => {
+  const {
+    state: { user, profileData, profileId },
+    dispatch: userDispatch,
+  } = useAuth();
+
+  const initialState = {
+    uid: user.uid,
+    about: profileData?.about || '',
+    email: user.email,
+    github: profileData?.github || '',
+    linkedIn: profileData?.linkedIn || '',
+    peerlist: profileData?.peerlist || '',
+    twitter: profileData?.twitter || '',
+    appliedJobs: profileData?.appliedJobs.map(item => item),
+    project: {
+      first: profileData?.project?.first || '',
+      second: profileData?.project?.second || '',
+      third: profileData?.project?.third || '',
+    },
+    skills: profileData?.skills,
+  };
+  const profileReducer = (state, action) => {
+    switch (action.type) {
+      case 'UPDATE_ABOUT': {
+        return { ...state, about: action.payload };
+      }
+      case 'UPDATE_GITHUB': {
+        return { ...state, github: action.payload };
+      }
+      case 'UPDATE_LINKEDIN': {
+        return { ...state, linkedIn: action.payload };
+      }
+      case 'UPDATE_PEERLIST': {
+        return { ...state, peerlist: action.payload };
+      }
+      case 'UPDATE_TWITTER': {
+        return { ...state, twitter: action.payload };
+      }
+      case 'UPDATE_PROJECT': {
+        return {
+          ...state,
+          project: {
+            ...state.project,
+            [action.payload.index]: action.payload.data,
+          },
+        };
+      }
+      case 'UPDATE_SKILLS': {
+        return { ...state, skills: [...action.payload] };
+      }
+      default:
+        return state;
+    }
+  };
+  const [state, dispatch] = useReducer(profileReducer, initialState);
+
+  async function handleSubmit(e) {
+    if (profileData === undefined) {
+      try {
+        const docRef = await addDoc(collection(db, 'Users'), {
+          ...state,
+        });
+      } catch (e) {
+        console.error('Error adding document: ', e);
+      }
+    } else {
+      try {
+        const collectionRef = await doc(db, 'Users', profileId);
+        updateDoc(collectionRef, { ...state });
+
+        const unsb = onSnapshot(collectionRef, doc => {
+          console.log(doc.data());
+          userDispatch({
+            type: 'SET_PROFILE_DATA',
+            payload: { data: doc.data(), profileId: doc.id },
+          });
+        });
+      } catch (err) {
+        console.log(err);
+      }
+      // const collectionRef = collection(db, 'Users');
+      // const q = query(collectionRef, where('uid', '==', user?.uid));
+    }
+  }
+
   return (
     <Flex justify="center" alignItems="center" direction="column" gap="4" p="6">
       <FormControl width={{ base: '100%', md: '50%', xl: '40%' }}>
         <Text fontSize="2xl">Enter Social Accounts</Text>
         <FormLabel>
           <Text fontSize="1xl">About</Text>
-          <Input placeholder="tell us more about yourself" isRequired />
+          <Input
+            placeholder="tell us more about yourself"
+            isRequired
+            value={state.about}
+            onChange={e =>
+              dispatch({ type: 'UPDATE_ABOUT', payload: e.target.value })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">Github</Text>
-          <Input placeholder="Enter Github Link" isRequired />
+          <Input
+            placeholder="Enter Github Link"
+            isRequired
+            value={state.github}
+            onChange={e =>
+              dispatch({ type: 'UPDATE_GITHUB', payload: e.target.value })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">LinkedIn</Text>
-          <Input placeholder="Enter LinkedIn Link" isRequired />
+          <Input
+            placeholder="Enter LinkedIn Link"
+            isRequired
+            value={state.linkedIn}
+            onChange={e =>
+              dispatch({ type: 'UPDATE_LINKEDIN', payload: e.target.value })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">PeerList</Text>
-          <Input placeholder="Enter PeerList Link" isRequired />
+          <Input
+            placeholder="Enter PeerList Link"
+            isRequired
+            value={state.peerlist}
+            onChange={e =>
+              dispatch({ type: 'UPDATE_PEERLIST', payload: e.target.value })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">Twitter</Text>
-          <Input placeholder="Enter Twitter Link" isRequired />
+          <Input
+            placeholder="Enter Twitter Link"
+            isRequired
+            value={state.twitter}
+            onChange={e =>
+              dispatch({ type: 'UPDATE_TWITTER', payload: e.target.value })
+            }
+          />
         </FormLabel>
 
         <Text fontSize="2xl">Add 3 Projects</Text>
         <FormLabel>
           <Text fontSize="1xl">First </Text>
-          <Input placeholder="Enter First Project Link" />
+          <Input
+            placeholder="Enter First Project Link"
+            value={state.project.first}
+            onChange={e =>
+              dispatch({
+                type: 'UPDATE_PROJECT',
+                payload: { index: 'first', data: e.target.value },
+              })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">Second</Text>
-          <Input placeholder="Enter Second Link" isRequired />
+          <Input
+            placeholder="Enter Second Link"
+            isRequired
+            value={state.project.second}
+            onChange={e =>
+              dispatch({
+                type: 'UPDATE_PROJECT',
+                payload: { index: 'second', data: e.target.value },
+              })
+            }
+          />
         </FormLabel>
         <FormLabel>
           <Text fontSize="1xl">Third</Text>
-          <Input placeholder="Enter Third Link" isRequired />
+          <Input
+            placeholder="Enter Third Link"
+            isRequired
+            value={state.project.third}
+            onChange={e =>
+              dispatch({
+                type: 'UPDATE_PROJECT',
+                payload: { index: 'third', data: e.target.value },
+              })
+            }
+          />
         </FormLabel>
-        <Flex gap="10" mt="1rem">
+        <Box>
+          <Creatable
+            defaultValue={
+              state.skills.length > 0
+                ? state?.skills.map(value => ({
+                    value: value,
+                    label: value,
+                  }))
+                : []
+            }
+            onChange={value =>
+              dispatch({
+                type: 'UPDATE_SKILLS',
+                payload: value.map(item => item.value),
+              })
+            }
+            options={options}
+            isMulti={true}
+            placeholder="Skills..."
+          />
+        </Box>
+        <Flex gap="10" mt="2rem">
           <Button colorScheme="teal" variant="outline" onClick={close}>
             Cancel
           </Button>
-          <Button colorScheme="blue">Submit</Button>
+          <Button onClick={handleSubmit} colorScheme="blue">
+            Submit
+          </Button>
         </Flex>
       </FormControl>
     </Flex>
--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -1,6 +1,8 @@
 import { onAuthStateChanged } from 'firebase/auth';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { createContext, useContext, useEffect, useReducer } from 'react';
-import { auth } from '../firebase/config';
+import { auth, db } from '../firebase/config';
+import useCollection from '../hooks/useCollection';
 
 const AuthContext = createContext();
 
@@ -8,6 +10,8 @@ const AuthProvider = ({ children }) => {
   const initialState = {
     user: null,
     isLoggedIn: false,
+    profileData: null,
+    profileId: null,
   };
   const authReducer = (state, action) => {
     switch (action.type) {
@@ -17,11 +21,19 @@ const AuthProvider = ({ children }) => {
       case 'LOGOUT': {
         return { ...state, user: null, isLoggedIn: false };
       }
+      case 'SET_PROFILE_DATA': {
+        return {
+          ...state,
+          profileData: action.payload.data,
+          profileId: action.payload.profileId,
+        };
+      }
       default:
         return state;
     }
   };
   const [state, dispatch] = useReducer(authReducer, initialState);
+  const { isLoggedIn, user } = state;
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, user => {
@@ -42,6 +54,26 @@ const AuthProvider = ({ children }) => {
 
     return () => unsub();
   }, []);
+
+  console.log(state);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const collectionRef = collection(db, 'Users');
+      const q = query(collectionRef, where('uid', '==', user?.uid));
+
+      getDocs(q).then(snapshot => {
+        const newData = snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }));
+        dispatch({
+          type: 'SET_PROFILE_DATA',
+          payload: { data: newData[0].data, profileId: newData[0].id },
+        });
+      });
+    }
+  }, [isLoggedIn, user]);
 
   console.log(state);
   return (
