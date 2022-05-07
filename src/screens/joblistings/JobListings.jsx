814e9d38169918a90f62c131fc84e072a1f76c76
prathmesh <prathmeshjagtap405@gmail.com>
2022-05-07 11:40:11 +0530

    job posting form
    added job posting form

--- src/screens/joblistings/JobListings.jsx
+++ src/screens/joblistings/JobListings.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { JobForm } from '../../components';
 
 function JobListings() {
-  return <div>JobListings Page</div>;
+  return (
+    <div>
+      <JobForm />
+    </div>
+  );
 }
 
 export { JobListings };
