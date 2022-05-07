5b5b04e25040b41f4c194bdcd0493f6899593ce8
vijaytembugade <vijaytembugade21@gmail.com>
2022-05-07 12:22:07 +0530

    accordian color

--- src/components/ReferalAccordian.jsx
+++ src/components/ReferalAccordian.jsx
@@ -15,12 +15,13 @@ function ReferalAccordian() {
       <Accordion
         defaultIndex={[0]}
         allowMultiple
+        allowToggle
         my="2"
         width={{ base: '100%', md: '40%' }}
       >
         <AccordionItem>
           <h2>
-            <AccordionButton>
+            <AccordionButton _expanded={{ bg: 'blue.400', color: 'black' }}>
               <Box flex="1" textAlign="left">
                 Following People have Asked for Referal
               </Box>
