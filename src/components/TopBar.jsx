b976ab374f3dd7de7f83061ab7eb7301d4f4f1da
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-09 01:48:17 +0530

    arrow icon change

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,13 +1,13 @@
 import { Flex, Heading, Avatar, Button } from '@chakra-ui/react';
 import { useSidebarContext } from '../contexts';
-import { ArrowBackIcon } from '@chakra-ui/icons';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 export const TopBar = ({ email }) => {
   const { onOpen, btnRef } = useSidebarContext();
   return (
     <Flex bg="gray.100" h="81px" w="100%" align="center" p={5}>
       <Button ref={btnRef} colorScheme="teal" onClick={onOpen} mr={3}>
-        <ArrowBackIcon />
+        <ArrowForwardIcon />
       </Button>
       <Avatar src="" name={email} mr={3} />
       <Heading size="lg">
