b657d21e23ae6d934d4a5ac4fa71d0cb5f7263b9
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-08 22:48:17 +0530

    Sidebar-context Added

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,2 +1,3 @@
 export { useAuth, AuthProvider } from './auth-context';
 export { JobProvider, useJobContext } from './job-context';
+export { SidebarProvider, useSidebarContext } from "./sidebar-context" ;
