75b54fb6f6f6d1b5407f3e8a66fa32cc1804c259
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-05-08 17:49:54 +0530

    Chat Functionality added

--- src/screens/Chat/Chat.jsx
+++ src/screens/Chat/Chat.jsx
@@ -1,38 +1,62 @@
 import { Flex, Heading, Text } from '@chakra-ui/react';
-import React from 'react';
+import { collection, doc, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
 import { BottomBar, SideBar, TopBar } from '../../components';
+import { useAuth } from '../../contexts';
+import { db } from '../../firebase/config';
+import { getOtherEmail } from '../../util/getOtherEmail';
+import {
+  useCollectionData,
+  useDocumentData,
+} from 'react-firebase-hooks/firestore';
 
 export const Chat = () => {
-  const Msg1 = () => {
-    return (
-      <Flex
-        bg="blue.100"
-        w="fit-content"
-        minWidth="100px"
-        borderRadius="lg"
-        // alignSelf="flex-end"
-        p={3}
-        m={1}
-      >
-        <Text>This is first msg</Text>
-      </Flex>
-    );
-  };
-  const Msg2 = () => {
-    return (
-      <Flex
-        bg="green.100"
-        w="fit-content"
-        minWidth="100px"
-        borderRadius="lg"
-        alignSelf="flex-end"
-        p={3}
-        m={1}
-      >
-        <Text>This is first msg</Text>
-      </Flex>
-    );
-  };
+  const { chatId } = useParams();
+  console.log(chatId);
+  const {
+    state: { user },
+  } = useAuth();
+
+  const [chat] = useDocumentData(doc(db, 'chats', chatId));
+  const q = query(
+    collection(db, `chats/${chatId}/messages`),
+    orderBy('timeAt')
+  );
+  const [messages] = useCollectionData(q);
+
+  const getMessages = () =>
+    messages?.map(msg => {
+      const sender = msg.sender === user.email;
+      return (
+        <Flex
+          key={Math.random()}
+          alignSelf={sender ? 'flex-start' : 'flex-end'}
+          bg={sender ? 'blue.100' : 'green.100'}
+          w="fit-content"
+          minWidth="100px"
+          borderRadius="lg"
+          p={3}
+          m={1}
+        >
+          <Text>{msg.text}</Text>
+        </Flex>
+      );
+    });
+
+  const bottomOfChat = useRef();
+  useEffect(
+    () =>
+      setTimeout(
+        bottomOfChat.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        }),
+        100
+      ),
+    []
+  );
+
   return (
     <div>
       <Flex h="100vh">
@@ -43,31 +67,21 @@ export const Chat = () => {
         <SideBar />
 
         <Flex flex={1} direction="column">
-          <TopBar />
-          {/* email={getOtherEmail(chat?.users, user)} */}
+          <TopBar email={getOtherEmail(chat?.users, user)} />
 
-          {/* <Flex flex={1} direction="column" pt={4} mx={5} overflowX="scroll" sx={{scrollbarWidth: "none"}}>
-          {getMessages()}
-          <div ref={bottomOfChat}></div>
-        </Flex> */}
-          <Flex flex={1} direction="column" pt={4} mx={5} overflowX="scroll" scrollbarWidth= "none">
-            <Msg2 />
-            <Msg1 />
-            <Msg2 />
-            <Msg1 />
-            <Msg1 />
-            <Msg1 />
-            <Msg2 />
-            <Msg1 />
-            <Msg2 />
-            <Msg1 />
-            <Msg1 />
-            <Msg1 />
-            <Msg1 />
-            <Msg2 />
+          <Flex
+            flex={1}
+            direction="column"
+            pt={4}
+            mx={5}
+            overflowX="scroll"
+            sx={{ scrollbarWidth: 'none' }}
+          >
+            {getMessages()}
+            <div ref={bottomOfChat}></div>
           </Flex>
-          <BottomBar />
-          {/* id={id} user={user} */}
+
+          <BottomBar id={chatId} user={user} />
         </Flex>
       </Flex>
     </div>
